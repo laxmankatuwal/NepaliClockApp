2b1c99152ed36efb539f2b88fc136581c03cb2c1
Laxman katuwal <katuwallaxman178@gmail.com>
2024-05-20 19:45:21 +0545

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import ClockHeading from "./components/ClockHeading";
+import ClockSlogan from "./components/ClockSlogan";
+import ClockTime from "./components/ClockTime";
+function App() {
+  return (
+    <center>
+      <ClockHeading />
+      <ClockSlogan />
+      <ClockTime />
+    </center>
+  );
+}
+export default App;
---
+++ ClockHeading.jsx
@@ -0,0 +1,5 @@
+let ClockHeading = () => {
+  return <h1 className="fw-border">Nepal Clock</h1>;
+};
+
+export default ClockHeading;
